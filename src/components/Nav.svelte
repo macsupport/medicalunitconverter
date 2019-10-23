9f4651f43a845ca0d816aa2461478340093f0132
Mike Sozanski <macsupport@gmail.com>
2019-10-23 12:44:37 -0700

    Initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let segment;
+</script>
+
+
+
+
+<nav class="w-full px-1">
+  <div class="container mx-auto rounded-t shadow bg-cover bg-center m-2" style="background-image: url(https://www.vetcalculators.com/img/emergencyBW.jpg)">
+ 	<!-- Text shadow utilities will be cool -->
+    <div class="flex justify-between p-4 items-center mb-10">
+      <a href="#" class=" text-black  text-sm md:text-4xl">Medical Unit Converter</a>
+      <ul class="list-reset flex">
+	   <li><a href="." class='bg-gray-700 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-l {segment === "home" ? "bg-blue-700" : ""}'>Home</a></li>
+        <li><a href="about" class='bg-gray-700 hover:bg-blue-700  text-white font-bold py-2 px-4  {segment === "about" ? "bg-blue-700" : ""}'>About</a></li>
+        <li><a href="legal" class='bg-gray-700 hover:bg-blue-700  text-white font-bold py-2 px-4  {segment === "legal" ? "bg-blue-700" : ""}'>Legal</a></li>
+        <li><a href="contact" class='bg-gray-700 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-r {segment === "contact" ? "bg-blue-700" : ""}'>Contact</a></li>
+      </ul>
+    </div>
+    
+    <div class="text-center text-black md:text-3xl pb-10">
+      <h1 class="font-sans"> US Units  SI Units Conversion</h1>
+    
+      <h2 class=" text-sm tracking-wide mt-2 mb-4">Common Lab Values</h2>
+  
+    </div>
+   
+  </div>
+  
+</nav>
---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>About Unit Conversion</title>
+</svelte:head>
+
+<h1 class="text-3xl">About Unit Conversion</h1>
+<p></p>
+<p class=""> The laboratory values and reference ranges are provided for illustration only and are not intended to be comprehensive or definitive. Each laboratory determines its own values, and reference ranges are highly method dependent.</p>
+ Sources: 
+ <ol>
+	 <li>Kratz A, Ferraro M, Sluss PM, Lewandrowski KB. Laboratory reference values. N Engl J Med. 2004;351(15):1548-1563</li>
+	 <li> Young DS, Huth EJ. SI Units for Clinical Measurement. Philadelphia, PA: American College of Physicians; 1998</li>
+	 <li> Henry JB, ed. Clinical Diagnosis and Management by Laboratory Methods. 20th ed. Philadelphia, PA: WB Saunders; 2001</li>
+	 <li>Kasper DL, Braunwald E, Fauci AS, et al, eds. Harrison's Principles of Internal Medicine, 16th ed. New York, NY: McGraw Hill; 2004</li>
+	 <li>Goldman L, Ausiello D. Cecil Textbook of Medicine. 22nd ed. Philadelphia, PA: WB Saunders; 2004</li>
+	 <li>atimer KS, Duncan & Prasse’s Veterinary Laboratory Medicine: Clinical Pathology, 5th Ed., Wiley-Blackwell, 2011</li>
+	 <li>Kaneko JJ, Harvey JW, Bruss ML, Clinical Biochemistry of Domestic Animals, 6th Ed., Academic Press, 2008</li>
+ </ol>
